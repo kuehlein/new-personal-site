9cb62d227f72e3d1c3a3598bc5b1f8f7a3cb69ff
Kyle Uehlein <kyleuehlein@gmail.com>
2019-02-15 22:28:28 -0500

    feat: update resume and site text

--- src/components/Home/Description.jsx
+++ src/components/Home/Description.jsx
@@ -4,10 +4,17 @@ import "./Home.css";
 const Description = () => (
   <div className="description">
     <p>
-      Kyle Uehlein is a full-stack Javascript developer. His greatest strength
-      is the ability to quickly and easily grasp new concepts and technologies
-      at a deep-level. Kyle taught himself Python and C#, and pursued a mastery
-      in web-development through his education at Fullstack Academy.
+      Kyle Uehlein is a full-stack software engineer fluent in Javascript and Typescript.
+      He is passionate about writing clean, maintainable, DRY code,
+      using the industry's best practices.
+
+      Kyle strongly believes in using the right tools for the job,
+      whether that means using cutting edge technologies or sticking to
+      what's reliable. In either case he's committed to quickly learn and
+      implement these concepts with a deep level of understanding.
+
+      Kyle taught himself Python, C#, and Typescript and pursued a mastery
+      in software-development through his education at Fullstack Academy.
     </p>
   </div>
 );
